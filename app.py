e0344a47be91ae06f83e3d1ef4423316cc72d02e
Valentin Quelquejay <valentin.quelquejay-leclere@epfl.ch>
2019-05-02 22:40:03 +0200

    basic table dump and template

--- app.py
+++ app.py
@@ -8,13 +8,26 @@ app.debug = True
 
 #database connection
 engine = create_engine('oracle://C##DB2019_G43:DB2019_G43@cs322-db.epfl.ch:1521/ORCLCDB')
-result = engine.execute("select bed_type from bed_type")
+connection = engine.connect()
+metadata = MetaData()
+#tables
+host = Table('HOST', metadata, autoload=True, autoload_with=engine)
+
+#db helper functions
+def dump_table(table) :
+    query = table.select()
+    ResultProxy = connection.execute(query)
+    result = ResultProxy.fetchmany(10)
+    ResultProxy.close()
+    return result
 
 @app.route('/')
 def get():
-    return render_template('index.html', result = result)
+    return render_template('index.html', columns = host.columns.keys(), records = dump_table(host))
 
 if __name__ == "__main__":
     app.run()
 
     
+
+
