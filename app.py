70b08b31e1ea66155363cf221692ec91300f8b73
Valentin Quelquejay <valentin.quelquejay-leclere@epfl.ch>
2019-05-02 08:32:19 +0200

    connected to oracle driver

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, MetaData, Table
+#general configuration
 app = Flask(__name__)
+#for hot reoading
+app.debug = True
+
+#database connection
+engine = create_engine('oracle://C##DB2019_G43:DB2019_G43@cs322-db.epfl.ch:1521/ORCLCDB')
+result = engine.execute("select bed_type from bed_type")
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def get():
+    return render_template('index.html', result = result)
 
 if __name__ == "__main__":
     app.run()
